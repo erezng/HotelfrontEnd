d1c460051bbb1f43c09ac7d28cd815afa0a34407
erez gabay <erez2g@gmail.com>
2023-02-14 19:11:17 +0200

    createaboutloggedin

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useContext } from "react"
+import { Navigate } from "react-router-dom"
+import AuthContext from "../context/AuthContext"
 
 const About = () => {
+  const {username}=useContext(AuthContext)
+  const token=localStorage.getItem("token")
+  if(!token){
+    return <Navigate to="/"/>;
+  }
+fetch("http://localhost:3001/api/books/all", {
+    headers: { "Authorization": token },
+  })
+    .then((res)=>res.json)
+    .then((data)=>{
+      console.log(data);
+    })
   return (
-    <div>About</div>
+    <div>Hello {username}</div>
   )
 }
 
