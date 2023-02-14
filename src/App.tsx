78d3b1bc56c72a6565791ea6930d59d421c762a6
erez gabay <erez2g@gmail.com>
2023-02-14 16:28:56 +0200

    showroutebystate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
@@ -8,16 +8,18 @@ import Home from './routes/Home';
 import About from './routes/About';
 import Login from './routes/Login';
 import Register from './routes/Register';
+import AuthContext from './context/AuthContext';
 
 function App() {
+  const {isLoggedIn} =useContext(AuthContext)
   return (
     <>
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
+          {!isLoggedIn &&<Route path="/login" element={<Login/>}/>}
+          {!isLoggedIn &&<Route path="/register" element={<Register/>}/>}
         </Routes>
     </>
     );
